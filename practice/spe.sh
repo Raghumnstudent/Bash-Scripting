ac00afea9d750265fb7b7478383db321328266e7
Raghu M N <nraghu@zeomega.com>
2023-08-14 09:34:48 +0530

    Crontab

---
+++ practice/spe.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo " take all positional arguments as single string $*"
+echo "set to number of positional arguments passed $#"
+
+echo "status of last command $?"
+
+echo "$1"
+
+echo "process id of $$"
+
+echo "process id of last background running job $!"
+
