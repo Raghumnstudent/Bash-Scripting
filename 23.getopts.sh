617abede20eba3e601523c9a57fd8b778b2120f3
Raghu M N <nraghu@zeomega.com>
2024-06-07 19:00:35 +0530

    Updated the script of getops

--- 23.getopts.sh
+++ 23.getopts.sh
@@ -5,16 +5,16 @@
 #Created date: Tuesday 18 July 2023 09:11:14 AM IST
 #Modified date:
 
-while getopts ":a:b:c" option; do
+while getopts "a:b:c" option; do
       case $option in
 	   a) 
-           echo "Option -a was provided with argument $OPTARG"
+           echo "Option -a will be the argument and accept the vaule and value is $OPTARG"
            ;;
            b)
-	   echo "Option -b was provided with argument $OPTARG"
+	   echo "Option -b will be the argument and accept the vaule and value is $OPTARG"
 	   ;;
            c)
-	   echo "Option -c was provided"
+	   echo "Option -c will be the flag"
 	   ;;
            \?)
            echo "Invalid option -$OPTARG" >&2
